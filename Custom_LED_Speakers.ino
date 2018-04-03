6d5242a7bd7d6026b93cbb4d1d2110ac6f6c5b05
Johannes Wikström <35334790+Vinylwalk3r@users.noreply.github.com>
2018-04-03 12:23:19 +0200

    added the basecode to build upon

--- Custom_LED_Speakers.ino
+++ Custom_LED_Speakers.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LedOutput 3 //The sound to the Subwoofer
 
+#define SoundInput A0 //Signal to the lights to blink
+
+int SoundIntensity; //the variable to hold the intensity of the analog sound signal
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  pinMode(SoundInput, INPUT);
+  
+  pinMode(LedOutput, OUTPUT);
+ 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-
+  
+  SoundIntensity = analogRead(SoundInput);  //reads the sound signal and saves it as a number between 0 and 1023
+  
+  analogWrite(LedOutput, SoundIntensity *15);  //assigns the output pin "LedOutput" the value of variable "SoundIntensity";
+  
+  
 }
