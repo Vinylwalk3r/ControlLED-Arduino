46fb173b3eee2210340b52bfe00bf6354913c96b
Johannes Wikström <35334790+Vinylwalk3r@users.noreply.github.com>
2018-04-03 14:15:05 +0200

    sound & starting spin
    installed the sound code and started work on spin effect. Installed a if, else if, else to choose the mode

--- Custom_LED_Speakers.ino
+++ Custom_LED_Speakers.ino
@@ -1,6 +1,21 @@
-#define LedOutput 3 //The sound to the Subwoofer
+// Right Subwoofer, clockwise, begins in upper right corner
+#define Right1 3
+#define Right2 4 
+#define Right3 5
+#define Right4 6 
 
-#define SoundInput A0 //Signal to the lights to blink
+// Left Subwoofer, clockwise, begins in upper right corner
+#define Left1 7 
+#define Left2 8
+#define Left3 9 
+#define Left4 10 
+
+// All the extra light, will be renamed if installed
+#define Extra1 11 
+#define Extra2 12
+
+
+#define Mic A0 //Input from the microphone
 
 int SoundIntensity; //the variable to hold the intensity of the analog sound signal
 
@@ -8,19 +23,69 @@ void setup()
 {
   Serial.begin(9600);
   
-  pinMode(SoundInput, INPUT);
-  
-  pinMode(LedOutput, OUTPUT);
- 
+  pinMode(Mic, INPUT);
+
+  //Right SBW
+  pinMode(Right1, OUTPUT);
+  pinMode(Right2, OUTPUT);
+  pinMode(Right3, OUTPUT);
+  pinMode(Right4, OUTPUT);
+  // Left SBW
+  pinMode(Left1, OUTPUT);
+  pinMode(Left2, OUTPUT);
+  pinMode(Left3, OUTPUT);
+  pinMode(Left4, OUTPUT);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
+
+ if (=true)
+ {
+  SoundIntensity = analogRead(Mic);  //reads the sound signal and saves it as a number between 0 and 1023
+
+  // Makes all the LEDs flash with the music. Adjust the number with * for better precision
+  analogWrite(Right1, SoundIntensity *15);  //assigns the pin the value of variable "SoundIntensity";
+  analogWrite(Right2, SoundIntensity *15);
+  analogWrite(Right3, SoundIntensity *15);
+  analogWrite(Right4, SoundIntensity *15);
+  analogWrite(Left1, SoundIntensity *15);
+  analogWrite(Left2, SoundIntensity *15);
+  analogWrite(Left3, SoundIntensity *15);
+  analogWrite(Left4, SoundIntensity *15); 
+  }
+ 
+  else if (=true)
+  {
+  digitalWrite(Right1, HIGH);
+  digitalWrite(Right2, HIGH;
+  digitalWrite(Right3, HIGH);
   
-  SoundIntensity = analogRead(SoundInput);  //reads the sound signal and saves it as a number between 0 and 1023
+  digitalWrite(Right1, LOW;
+  digitalWrite(Right4, HIGH);
+  digitalWrite(Right2, LOW);
   
-  analogWrite(LedOutput, SoundIntensity *15);  //assigns the output pin "LedOutput" the value of variable "SoundIntensity";
+  digitalWrite(Right1, HIGH);
+  digitalWrite(Right3, LOW;
+  digitalWrite(Right2, HIGH);
   
+  digitalWrite(Right4, HIGH;
+  digitalWrite(Right3, LOW);
+  digitalWrite(Right2, HIGH;
+  
+  digitalWrite(Right1, HIGH);
+  digitalWrite(Right2, HIGH;
+  }
+ 
+  else if (=true)
+  {
+  
+  }
+  else 
+  {
+  
+}
+  
+ 
   
 }
