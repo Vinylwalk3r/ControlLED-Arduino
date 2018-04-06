c46a4b7b9744743f37aa50b70f9062acc4832553
Johannes Wikström <35334790+Vinylwalk3r@users.noreply.github.com>
2018-04-06 14:52:54 +0200

    finished the temp intervals and built the manual override
    made the fans controllable both using automatic temp control and manual override using the FanRegulator

--- Custom_LED_Speakers.ino
+++ Custom_LED_Speakers.ino
@@ -1,3 +1,5 @@
+#define aref_voltage 3.3 // tie 3.3V to ARef
+
 // Right Subwoofer, clockwise, begins in upper right corner
 #define Right1 3
 #define Right2 4 
@@ -15,6 +17,7 @@
 #define TempSensor A1 // Temperature sensor for the amplifiers
 
 #define FanRegulator A2 // Manual regulator for the fans
+#define FanSpeed A3 // connect the fans here
 
 #define Mic A0 //Input from the microphone
 
@@ -28,8 +31,7 @@ int lastButtonState = 0;     // Previous state of the button
 
 // Variables for the 
 int currentTemp; // Stores the current temperature
-
-#define aref_voltage 3.3 // tie 3.3V to ARef
+int manualFanSpeed; // Holds the reading from the fanregulator
 
 void setup() 
 {
@@ -43,7 +45,7 @@ void setup()
   pinMode(FanRegulator, INPUT);
   pinMode(Mic, INPUT);
 
-  pinMode(Fans, OUTPUT);
+  pinMode(FanSpeed, OUTPUT);
 
   //Right SBW
   pinMode(Right1, OUTPUT);
@@ -150,16 +152,36 @@ voltage /= 1024.0;
 //converting from 10 mv per degree wit 500 mV offset to degrees ((volatge - 500mV) times 100)
 float temperatureC = (voltage - 0.5) * 100 ;  
 
-if (temperatureC >= 20)
-{
-  analogWrite(Fans)
-
-
+// checks if the manual override regulator has been used. If not, cthis code runs (automatic fan control)
+if (FanRegulator = 0) {
+  if (temperatureC >= 19) { //turns of the fans at 19 degress Celsius or below
+    analogWrite(FanSpeed, 0)
+  }
+  else if (temperatureC >= 20) { // fans at 5% at 20c or under
+    analogWrite(FanSpeed, 50)
+  }
+  else if (temperatureC >= 25) { // ~20% fanspeed at 25c
+    analogWrite(FanSpeed, 250)
+  }
+  else if (temperatureC >= 30) { // 40% fanspeed at 30c
+    analogWrite(FanSpeed, 400)
+  }
+  else if (temperatureC >= 35) { // 60% fanspeed at 35c
+    analogWrite(FanSpeed, 600)
+  }
+  else if (temperatureC >= 45) { // 80% fanspeed at 40c
+    analogWrite(FanSpeed, 800)
+  }
+  else if (temperatureC >= 50) { //fans at full speed at 50 degress Celsius
+    analogWrite(FanSpeed, 1023)
+  }
 }
 
+else { // if the manual override regulator has been moved, this code runs (manual fan control)
+ manualFanSpeed = analogRead(FanRegulator); // reads the setting of the fan regulator and stores it in manualFanSpeed
 
-
-
+ analogWrite(FanSpeed, manualFanSpeed); // assigns the values of manualFanSpeed to the fans
+}
 
 
 }
