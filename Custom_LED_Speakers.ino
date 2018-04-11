03fcf7060816c8fe738ef20d816a2563e55cbcd3
Johannes Wikström <35334790+Vinylwalk3r@users.noreply.github.com>
2018-04-11 10:28:09 +0200

    redesigned & added massDigitalWrite
    optimized the code, added massdigitalWrite and made FOR loops for segments of mass digitalwrite of pinmode

--- Custom_LED_Speakers.ino
+++ Custom_LED_Speakers.ino
@@ -2,15 +2,15 @@
 
 // Right Subwoofer, clockwise, begins in upper right corner
 #define Right1 3
-#define Right2 4 
+#define Right2 4
 #define Right3 5
-#define Right4 6 
+#define Right4 6
 
 // Left Subwoofer, clockwise, begins in upper right corner
-#define Left1 7 
+#define Left1 7
 #define Left2 8
-#define Left3 9 
-#define Left4 10 
+#define Left3 9
+#define Left4 10
 
 #define ModeSwitch 12 // Button for switching profile
 
@@ -41,12 +41,14 @@ unsigned long previousMillisButton = 0; // time since last run of ButtonCheck
 // constants won't change:
 const long interval = 1000; // interval at which to do TempCheck (milliseconds)
 
-// unsigned char pins = {2, 3, 4,}; // write all pins you want to be able to call here
+byte pins[] = {3, 4, 5, 6, 7, 8, 9, 10};
 
-void setup() 
+int data;
+
+void setup()
 {
   Serial.begin(9600); // Serial used for bluetooth
-  
+
   // If you want to set the aref to something other than 5v
   analogReference(EXTERNAL);
 
@@ -57,39 +59,30 @@ void setup()
 
   pinMode(FanSpeed, OUTPUT);
 
-  //Right SBW
-  pinMode(Right1, OUTPUT);
-  pinMode(Right2, OUTPUT);
-  pinMode(Right3, OUTPUT);
-  pinMode(Right4, OUTPUT);
-  // Left SBW
-  pinMode(Left1, OUTPUT);
-  pinMode(Left2, OUTPUT);
-  pinMode(Left3, OUTPUT);
-  pinMode(Left4, OUTPUT); 
+  //Right & Left SB
+  for (int i = 0; i < sizeof(pins); i++)
+  {
+    pinMode(pins[i], OUTPUT); //makes all LED pins outputs
+  }
 }
 
-/* void massDigitalWrite(unsigned char pins[], unsigned char length, unsigned char state) // Call this to make a mass digitalWrite
-{
- unsigned char i; // Creates a temp char i
- for (i = 0; i < length; i++) // for loop that runs infinitly
-   digitalWrite(pins, state); // a digitalWrite which gets its pin number and state sent to it when its called
+void massDigitalWrite() {
+  for (int i = 0; i < sizeof(pins); i++)
+  {
+    digitalWrite(pins[i], LOW); //declaring them as outputs
+  }
 }
-*/
+
 void FlashingSound() // flashes the LEDs in beat with the music
 {
   //reads the sound signal and saves it as a number between 0 and 1023
-  SoundIntensity = analogRead(Mic);  
+  SoundIntensity = analogRead(Mic);
 
   // Makes all the LEDs flash with the music. Adjust the number with * for better precision
-  analogWrite(Right1, SoundIntensity *15);  //assigns the pin the value of variable "SoundIntensity";
-  analogWrite(Right2, SoundIntensity *15);
-  analogWrite(Right3, SoundIntensity *15);
-  analogWrite(Right4, SoundIntensity *15);
-  analogWrite(Left1, SoundIntensity *15);
-  analogWrite(Left2, SoundIntensity *15);
-  analogWrite(Left3, SoundIntensity *15);
-  analogWrite(Left4, SoundIntensity *15); 
+for (int i = 0; i < sizeof(pins); i++)
+ {
+   digitalWrite(pins[i], SoundIntensity * 15); //assigns the pin the value of variable "SoundIntensity";
+ }
 }
 
 void SpinEffect() //creates a infinte loopable spin effect
@@ -98,22 +91,22 @@ void SpinEffect() //creates a infinte loopable spin effect
   digitalWrite(Left4, HIGH);
   digitalWrite(Right2, LOW);
   digitalWrite(Left2, LOW);
-  
+
   digitalWrite(Right1, HIGH);
   digitalWrite(Left1, HIGH);
   digitalWrite(Right3, LOW);
   digitalWrite(Left3, LOW);
-  
+
   digitalWrite(Right2, HIGH);
   digitalWrite(Left2, HIGH);
   digitalWrite(Right4, LOW);
   digitalWrite(Left4, LOW);
-  
+
   digitalWrite(Right3, HIGH);
   digitalWrite(Left3, HIGH);
   digitalWrite(Right1, LOW);
   digitalWrite(Left1, LOW);
-  
+
   digitalWrite(Right4, HIGH);
   digitalWrite(Left4, HIGH);
   digitalWrite(Right2, LOW);
@@ -125,43 +118,42 @@ void RandomEffect() // Lights the LEDs in a random sequense
   LeftRandom = random(4);
   RightRandom = random(4);
 
-//  massDigitalWrite(pins, Right1, HIGH);
+  massDigitalWrite();
 
-    if (LeftRandom = 1) {
-  digitalWrite(Left1, HIGH);
+  if (LeftRandom = 1) {
+    digitalWrite(Left1, HIGH);
   }
-    if (LeftRandom = 2) {
-  digitalWrite(Left2, HIGH);
+  if (LeftRandom = 2) {
+    digitalWrite(Left2, HIGH);
   }
-    if (LeftRandom = 3) {
-  digitalWrite(Left3, HIGH);
+  if (LeftRandom = 3) {
+    digitalWrite(Left3, HIGH);
   }
-    if (LeftRandom = 4) {
-  digitalWrite(Left4, HIGH);
+  if (LeftRandom = 4) {
+    digitalWrite(Left4, HIGH);
   }
-    if (RightRandom = 1) {
-  digitalWrite(Right1, HIGH);
+  if (RightRandom = 1) {
+    digitalWrite(Right1, HIGH);
   }
-    if (RightRandom = 2) {
-  digitalWrite(Right2, HIGH);
+  if (RightRandom = 2) {
+    digitalWrite(Right2, HIGH);
   }
-    if (RightRandom = 2) {
-  digitalWrite(Right2, HIGH);
+  if (RightRandom = 2) {
+    digitalWrite(Right2, HIGH);
   }
-    if (RightRandom = 3) {
-  digitalWrite(Right3, HIGH);
+  if (RightRandom = 3) {
+    digitalWrite(Right3, HIGH);
   }
-    if (RightRandom =41) {
-  digitalWrite(Right4, HIGH);
+  if (RightRandom = 4) {
+    digitalWrite(Right4, HIGH);
   }
-      
 }
 
-void TempCheck() { 
-// call this to check the temps and control the fans, 
-//comment this part out if you dont have a temp sensor - fan control setup.
+void TempCheck() {
+  // call this to check the temps and control the fans,
+  //comment this part out if you dont have a temp sensor - fan control setup.
 
-// checks if the manual override regulator has been used. If not, this code runs (automatic fan control)
+  // checks if the manual override regulator has been used. If not, this code runs (automatic fan control)
   manualFanSpeed = analogRead(FanRegulator); // reads the setting of the fan regulator and stores it in manualFanSpeed
 
   if (manualFanSpeed = 0) {
@@ -169,10 +161,10 @@ void TempCheck() {
 
     // converting that reading to voltage, which is based off the reference voltage
     float voltage = currentTemp * aref_voltage;
-    voltage /= 1024.0; 
- 
+    voltage /= 1024.0;
+
     //converting from 10 mv per degree wit 500 mV offset to degrees ((volatge - 500mV) times 100)
-    float temperatureC = (voltage - 0.5) * 100 ;  
+    float temperatureC = (voltage - 0.5) * 100 ;
 
     if (temperatureC >= 19) { //turns of the fans at 19 degress Celsius or below
       analogWrite(FanSpeed, 0);
@@ -204,44 +196,43 @@ void TempCheck() {
 void ButtonCheck() // call this to check the button states
 {
   buttonState = digitalRead(ModeSwitch); // This part checks the button and counts how many times the button has been pushed
- 
-    if (buttonState != lastButtonState) { // compare the buttonState to its previous state
-      
-      if (buttonState == HIGH) {  // if the state has changed, increment the counter
-        buttonPushCounter++;  // if the current state is HIGH then the button went from off to on
-      } 
-    
+
+  if (buttonState != lastButtonState) { // compare the buttonState to its previous state
+
+    if (buttonState == HIGH) {  // if the state has changed, increment the counter
+      buttonPushCounter++;  // if the current state is HIGH then the button went from off to on
+    }
     if (buttonPushCounter = 5)
     {
-    buttonPushCounter = 0;
+      buttonPushCounter = 0;
     }
   }
-  
+
   lastButtonState = buttonState;   // save the current state as the last state, for next time through the loop
 }
 
 
 void loop()
 {
-unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
 
-  if(Serial.available() > 0)  // Send data only when you receive data:
+  if (Serial.available() > 0) // Send data only when you receive data:
   {
     data = Serial.read();  //Read the incoming data & store into data
     Serial.print(data);  //Print Value inside data in Serial monitor
-    Serial.print("\n");        
+    Serial.print("\n");
   }
 
   if (currentMillis - previousMillisTemp >= interval) {
     // save the last time the code ran
     previousMillisTemp = currentMillis;
-  
-  TempCheck();
-  ButtonCheck();
+
+    TempCheck();
+    ButtonCheck();
   }
 
   // this checks the value of buttonPushCounter and selects the appropriate effect
-  if (buttonPushCounter = 1) {
+  if (buttonPushCounter = 1 || data == 8) { // || means or, and we are gonna have to figure out a way to send commands from the app
     FlashingSound;
   }
   if (buttonPushCounter = 2) {
@@ -252,16 +243,21 @@ unsigned long currentMillis = millis();
   }
   /* if (buttonPushCounter = 4); {
     //place new effect here
-  }*/
+    }*/
 }
 
+/*
+   for (int i = 0; i < sizeof(segments); i++)
+  {
+    digitalWrite(segments[i], SoundIntensity * 15); //assigns the pin the value of variable "SoundIntensity";
+  }
+ */
 
 
 
-
-   /*
-         if(data == '1')              // Checks whether value of data is equal to 1
-         digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
-      else if(data == '0')         //  Checks whether value of data is equal to 0
-         digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
-         */
+/*
+      if(data == '1')              // Checks whether value of data is equal to 1
+      digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
+   else if(data == '0')         //  Checks whether value of data is equal to 0
+      digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+*/
